b16c58af79a0468cb30b90897e6539fc8ede83bc
Quinntyne Brown <quinntynebrown@gmail.com>
2016-09-29 08:58:11 -0400

    initial code commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+﻿var gulp = require('gulp');
+var tsc = require('gulp-typescript');
+var inlineNg2Template = require('gulp-inline-ng2-template');
+var Config = require('./gulpfile.config');
+var config = new Config();
+var clean = require('gulp-clean');
+var del = require('del');
+var webpack = require("webpack-stream");
+
+gulp.task('clean', function () {
+    var tsGeneratedFiles = [
+        'src/lib/schedule.component.d.ts',
+        'src/lib/schedule.component.js',
+        'src/lib/index.d.ts',
+        'src/lib/index.js'
+    ];
+
+    return gulp.src(tsGeneratedFiles, { read: false })
+        .pipe(clean());
+});
+
+gulp.task('compile', ['clean'], function () {
+    var sourceTsFiles = [
+        './src/lib/src/schedule.component.ts',
+        './src/lib/src/index.ts',
+        config.libraryTypeScriptDefinitions
+    ];
+
+    var tsResult = gulp.src(sourceTsFiles)
+        .pipe(inlineNg2Template({ base: '/src/lib/src/' }))
+        .pipe(tsc(config.tsConfig));
+
+    tsResult.dts.pipe(gulp.dest('./src/lib'));
+    return tsResult.js
+                    .pipe(gulp.dest('./src/lib'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(
+        [
+            './src/lib/src/schedule.component.ts',
+            './src/lib/src/schedule.component.html',
+            './src/lib/src/schedule.component.css',
+            './src/lib/src/index.ts',
+        ],
+        ['clean','compile']);
+});
+
+gulp.task('default', ['clean', 'compile', 'watch']);
+
+
---
+++ src/lib/src/schedule.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    templateUrl: "./schedule.component.html",
+    styleUrls: ["./schedule.component.css"],
+    selector: "schedule"
+})
+export class ScheduleComponent { }
