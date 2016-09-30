25bad2c44aaa2b044f3d2a0165ddb9110d14ed7b
Quinntyne Brown <quinntynebrown@gmail.com>
2016-09-30 04:52:39 -0400

    day-picker

--- src/demo-app/app/schedule/day-picker.component.ts
+++ src/demo-app/app/schedule/day-picker.component.ts
@@ -22,31 +22,56 @@ export const translateX = (element: HTMLElement, value: number) => {
     styles: [require("./day-picker.component.scss")],
     selector: "day-picker"
 })
-export class DayPickerComponent implements AfterViewInit { 
+export class DayPickerComponent implements AfterViewInit, OnInit { 
     constructor(private _elementRef: ElementRef) {}
     
     @Input()
     public days: Array<Day> = [];
 
     private _index: number;
-    private delta: number;
-    ngAfterViewInit() {
-        this.delta = (((this.days.length * 75) - this.width) / 2) * -1;
-        translateX(this._elementRef.nativeElement.firstElementChild.firstElementChild, this.delta);
+    
+    get cellWidth() { return 85; }
+
+    get cellsWidth() { return this.cellWidth * this.days.length; }
+
+    get width() { return this._elementRef.nativeElement.getBoundingClientRect()["width"]; }
+    
+    public onDayPicked: EventEmitter<any> = new EventEmitter();
+
+    ngOnInit() { this._index = Math.floor(this.days.length / 2); }
+
+    ngAfterViewInit() {                
+        this.changeIndex(this._index);
         setTimeout(() => {
-            this._elementRef.nativeElement.firstElementChild.firstElementChild.style["transition"] = "all 1s";
+            this._elementRef.nativeElement.firstElementChild.firstElementChild.style["transition"] = "all 500ms";
         }, 0)
         
     }
 
+    public get endPosition() { return (this.width - (this.days.length * this.cellWidth)); }
+
     onClick(index: number) {      
-        this._index = index;    
-        translateX(this._elementRef.nativeElement.firstElementChild.firstElementChild, ((index - 8) * -75) - this.delta);
+        this.changeIndex(index); 
+        this.onDayPicked.emit({
+            index: index,
+            day: this.days[index]
+        });
     }
 
-    onResize($event) {
-        
+    public changeIndex(index: number) {
+        this._index = index;
+        var cellIndex = (index * this.cellWidth) + (this.cellWidth / 2);
+        var delta = (cellIndex - (this.width / 2)) * -1;
+
+        if (delta > 0)
+            delta = 0;
+
+        if (delta < this.endPosition)
+            delta = this.endPosition;
+
+        translateX(this._elementRef.nativeElement.firstElementChild.firstElementChild, delta); 
     }
 
-    get width() { return this._elementRef.nativeElement.getBoundingClientRect()["width"]; }
+    onResize($event) { this.changeIndex(this._index); }
+    
 }
