c1d115c1fef7eeceec37e4e08a08496cc2abe6f3
Quinntyne Brown <quinntynebrown@gmail.com>
2016-09-29 20:31:57 -0400

    day-picker

--- src/demo-app/app/schedule/day-picker.component.ts
+++ src/demo-app/app/schedule/day-picker.component.ts
@@ -1,21 +1,52 @@
-import { Component, ChangeDetectionStrategy, Input, OnInit } from "@angular/core";
+import {
+    Component,
+    ChangeDetectionStrategy,
+    EventEmitter,
+    Input,
+    OnInit,
+    ElementRef,
+    AfterViewInit
+} from "@angular/core";
 
 export interface Day {
     dayOfWeek: string;
     date: string;
 }
 
+export const translateX = (element: HTMLElement, value: number) => {
+    element.style["transform"] = `translateX(${value}px)`;
+}
+
 @Component({
     template: require("./day-picker.component.html"),
     styles: [require("./day-picker.component.scss")],
-    selector: "day-picker",
-    changeDetection: ChangeDetectionStrategy.OnPush
+    selector: "day-picker"
 })
-export class DayPickerComponent implements OnInit { 
-    ngOnInit() {
-
-    }
-
+export class DayPickerComponent implements AfterViewInit { 
+    constructor(private _elementRef: ElementRef) {}
+    
     @Input()
     public days: Array<Day> = [];
+
+    private _index: number;
+    private delta: number;
+    ngAfterViewInit() {
+        this.delta = (((this.days.length * 75) - this.width) / 2) * -1;
+        translateX(this._elementRef.nativeElement.firstElementChild.firstElementChild, this.delta);
+        setTimeout(() => {
+            this._elementRef.nativeElement.firstElementChild.firstElementChild.style["transition"] = "all 1s";
+        }, 0)
+        
+    }
+
+    onClick(index: number) {      
+        this._index = index;    
+        translateX(this._elementRef.nativeElement.firstElementChild.firstElementChild, ((index - 8) * -75) - this.delta);
+    }
+
+    onResize($event) {
+        
+    }
+
+    get width() { return this._elementRef.nativeElement.getBoundingClientRect()["width"]; }
 }
